4a96d2704e579749c6411ce373e34645fe53a041
Medhat Elmasry <medhat@elmasry.ca>
2018-04-15 20:41:59 -0700

    Update CompoundInterest.cs
    commented out a rule in the model class

--- Calculator/Models/CompoundInterest.cs
+++ Calculator/Models/CompoundInterest.cs
@@ -12,14 +12,15 @@ namespace Calculator.Models
 
         public double Principal {
             get { return _principal; }
-            set {
-                if (value < 0)
-                {
-                    throw new Exception("Principal amount cannot be negative.");
-                }
+            
+//             set {
+//                 if (value < 0)
+//                 {
+//                     throw new Exception("Principal amount cannot be negative.");
+//                 }
 
-                _principal = value;
-            }
+//                 _principal = value;
+//             }
         }
 
         [Display(Name = "Times interest is compounded per year")]
